f8785d004ba24020f32e0ae36406ec0e97c4fc61
Oskar Blom <oskar.blom@gmail.com>
2012-06-05 17:36:22 +0200

    Switch to python

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Uppsalabarn::Application.config.secret_token = '9af5dc718c7129c9110add3d2eab48e58dcaf23b20dcd7c7a938ccb31f4e58c8dd679c15642983885fc398e3fe6f8ea3a3a6823bf735e28c796a461ee06a0ae0'
--- doc/sources.md
+++
@@ -1,7 +0,0 @@
-unt.se
-======
-http://www.unt.se/guiden/
-nickis
-======
-http://www.nickis.nu/index.php?option=com_content&view=article&id=10&Itemid=7
-
