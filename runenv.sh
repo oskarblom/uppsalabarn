7a95a621b1e4447f0f054cc79eb1ddcd1e2b67ba
Oskar Blom <oskar.blom@gmail.com>
2012-08-09 20:07:12 +0200

    Add env script

--- runenv.sh
+++ runenv.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+cd /Users/oskar/Projects/uppsalabarn
+workon uppsalabarnenv
 
+tmux new-session -d -s uppsalabarn
+tmux new-window -t uppsalabarn:1 -n "mongo" "startmongo"
+tmux new-window -t uppsalabarn:2 -n "sass" "sass --watch site.scss:static/css/site.css"
